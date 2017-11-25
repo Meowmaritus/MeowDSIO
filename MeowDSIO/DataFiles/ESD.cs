11cddd98a705bb78659a512e9bb9b1dbd371bc83
Meowmaritus <derp250@gmail.com>
2017-11-25 17:46:34 -0600

    Add very vague template for ESD that does NOT work AT ALL yet

---
+++ MeowDSIO/DataFiles/ESD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowDSIO.DataFiles
+{
+    public class ESD : DataFile
+    {
+
+
+        protected override void Read(DSBinaryReader bin)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Write(DSBinaryWriter bin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
