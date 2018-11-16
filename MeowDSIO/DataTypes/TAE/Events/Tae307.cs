3377ae68563b095adf568059c3f8e61dde16a76d
Meowmaritus <Meowmaritus@gmail.com>
2018-11-16 11:26:16 -0600

    tae stuff

--- MeowDSIO/DataTypes/TAE/Events/Tae307.cs
+++ MeowDSIO/DataTypes/TAE/Events/Tae307.cs
@@ -21,18 +21,21 @@ namespace MeowDSIO.DataTypes.TAE.Events
                 UNK1,
                 UNK2,
                 UNK3,
+                UNK4,
             };
         }
 
-        public int UNK1 { get; set; } = 0;
-        public int UNK2 { get; set; } = 0;
+        public short UNK1 { get; set; } = 0;
+        public short UNK2 { get; set; } = 0;
         public int UNK3 { get; set; } = 0;
+        public int UNK4 { get; set; } = 0;
 
         public override void ReadParameters(DSBinaryReader bin)
         {
-            UNK1 = bin.ReadInt32();
-            UNK2 = bin.ReadInt32();
+            UNK1 = bin.ReadInt16();
+            UNK2 = bin.ReadInt16();
             UNK3 = bin.ReadInt32();
+            UNK4 = bin.ReadInt32();
         }
 
         public override void WriteParameters(DSBinaryWriter bin)
@@ -40,6 +43,7 @@ namespace MeowDSIO.DataTypes.TAE.Events
             bin.Write(UNK1);
             bin.Write(UNK2);
             bin.Write(UNK3);
+            bin.Write(UNK4);
         }
 
         protected override TimeActEventType GetEventType()
